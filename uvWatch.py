0154a67086bd0bdf770d0ca5ec894d80198b9ad4
John Rogers <k1wiz@wizworks.net>
2023-02-10 11:34:04 -0500

    Update uvWatch.py

--- uvWatch.py
+++ uvWatch.py
@@ -10,7 +10,7 @@ def check_uv():
 
     while True:
         try:
-            with open('/scripts/wxresult', 'r') as f:
+            with open('/scripts/wxresult', 'r') as f:  # pickup json data from temp file
                 try:
                     data = json.load(f)
                 except json.decoder.JSONDecodeError as e:
@@ -26,18 +26,17 @@ def check_uv():
 
     try:
         uv_level = data['obs'][0]['uv']
-        if uv_level >= 10:   # Set desired threshold here. A UV index of 10 is considered dangerous.
+        if uv_level >= 10:      # Set UV threshold here
             now = datetime.datetime.now()
             from_address = "" # Set your FROM address here
             to_address = "" # Set your destination address here
             subject = f"Dangerous UV Alert: ({now.strftime('%m-%d-%Y %I:%M %p')})"
-            message = f"THIS IS AN AUTOMATED WARNING - DO NOT REPLY \nWeather sensors have detected DANGEROUS UV LEVELS at or \
-                greater than {uv_level} in our neighborhood.  If outdoors or poolside, please consider moving to safe shelter \
-                immediately to avoid dangerous exposure.  Re-assess conditions after 1 hour from the time of this alert.  If \
-                no more alerts are received after 1 hour has passed, conditions should have expired and you should proceed \
-                outdoors with caution.  If conditions have not expired within 1 hour from this alert, you will receive another \
-                message if the unsafe condition is continued 1 hour from now\n\nPlease tune in to local broadcast outlets to \
-                get the latest information on weather conditions.  \n\nSTAY SAFE!"
+            message = f"THIS IS AN AUTOMATED WARNING - DO NOT REPLY \nWeather sensors have detected DANGEROUS UV LEVELS at or greater \
+than {uv_level}  in our neighborhood.  If outdoors or poolside, please consider moving to safe shelter. \
+Re-assess conditions after 1 hour from the time of this alert.  If no more alerts are received after 1 hour has passed, conditions \
+should have expired and you should proceed outdoors with caution.  If conditions have not expired within 1 hour from this alert, \
+you will receive another message if the unsafe condition is continued 1 hour from now\n\nPlease tune in to local broadcast outlets to \
+get the latest information on weather conditions.  \n\nSTAY SAFE!"
             smtp_server = ""  # Put your SMTP relay here
             email_message = f"From: {from_address}\nTo: {to_address}\nSubject: {subject}\n\n{message}"
             with smtplib.SMTP(smtp_server) as server:
@@ -47,8 +46,6 @@ def check_uv():
             condition_flag = 0
     except KeyError:
         print("KeyError: Requested key not found in API response")
-
-current_hour = datetime.datetime.now().hour
 
 while True:
     now = datetime.datetime.now()
